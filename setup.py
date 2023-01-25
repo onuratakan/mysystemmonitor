24249333ed9f89299961974603ff21d45cc0937d
Onur Atakan ULUSOY <atadogan06@gmail.com>
2023-01-25 00:49:25 +0000

    new version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(name='mysystemmonitor',
-version='0.1.0',
+version='0.2.0',
 description="""A library to monitoring system.""",
 long_description="""
 # My System Monitor
